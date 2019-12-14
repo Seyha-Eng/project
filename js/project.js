0b0c52147e1636e49c0378a4ba2e9b809695aa8f
seyhaeng <seyha.eng@PNC.LAN>
2019-12-14 14:04:50 +0700

    update project

--- js/project.js
+++ js/project.js
@@ -5,11 +5,11 @@ function getURl() {
 $(document).ready(function(){   
     requestApi();
     $('#recipe').on('change',()=>{
-        var choseRecipe = $('#recipe').val();
-        getRecipe(chooseRecipe);
+        var recipeId = $('#recipe').val();
+        getRecipe(recipeId);
     })
 });
-
+//request API
 function requestApi(){
     $.ajax({
         dataType: 'json',
@@ -18,17 +18,52 @@ function requestApi(){
         error:() => console.log("Cannot get data"),
     })
 }
-var allData = [] //Globle array 
+var allData = [];
 function chooseRecipe(recipe){
     allData = recipe;
     var option = "";
-    recipe.forEach(item => {
-        option += `<option value="${item.id}">${item.name}</option>`;
+    recipe.forEach(element => {
+        option += `<option value="${element.id}">${element.name}</option>`;
     });
     $('#recipe').append(option);
 }
-function getRecipe(recipe){
-    switch(recipe){
-        
-    }
+//
+function getRecipe(id){
+    allData.forEach(item =>{
+        console.log(item);
+        if(item.id == id){
+            eachRecipe(item.name,item.iconUrl);
+            input(item.nbGuests);
+            eachIngredient(item.ingredients);
+        }
+    })
 }
+function eachRecipe(name,img){
+    var result ="";
+    result +=`
+    <h1>${name}</h1>
+    <img src ="${img}" width ="100">
+    `;
+    $('#recipe-result').html(result);
+}
+//get ingredient
+function eachIngredient(ingredients){
+    result = "";
+    ingredients.forEach(element => {
+        result += `
+        <tr>
+            <td><img src="${element.iconUrl}" class="img-fluid" width="50px"></td>
+            <td>${element.quantity + " " + element.unit[0] }</td>
+            <td>${element.name}</td>
+        </tr>
+        `;
+    });
+    $("#ingredient").html(result);
+}
+function input(member){
+    var result = "";
+    result += `    
+    `;
+    $("#input").html(result);
+}
+
