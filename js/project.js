7a844b943f1c39f0b3ca54a98ff726623fc56053
seyhaeng <seyha.eng@pnc.lan>
2019-12-17 09:41:33 +0700

    update project

--- js/project.js
+++ js/project.js
@@ -1,25 +1,31 @@
 function getURl() {
-    var url =  "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
+    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
-$(document).ready(function(){   
+$(document).ready(function () {
     requestApi();
-    $('#recipe').on('change',()=>{
+    $('#recipe').on('change', () => {
         var recipeId = $('#recipe').val();
         getRecipe(recipeId);
+        $("#plus").on("click", function(){
+            increases();
+        }); 
+        $("#minus").on('click', function(){
+            descreases();
+        })
     })
 });
 //request API
-function requestApi(){
+function requestApi() {
     $.ajax({
         dataType: 'json',
         url: getURl(),
         success: (data) => chooseRecipe(data.recipes),
-        error:() => console.log("Cannot get data"),
+        error: () => console.log("Cannot get data"),
     })
 }
 var allData = [];
-function chooseRecipe(recipe){
+function chooseRecipe(recipe) {
     allData = recipe;
     var option = "";
     recipe.forEach(element => {
@@ -28,42 +34,95 @@ function chooseRecipe(recipe){
     $('#recipe').append(option);
 }
 //
-function getRecipe(id){
-    allData.forEach(item =>{
-        console.log(item);
-        if(item.id == id){
-            eachRecipe(item.name,item.iconUrl);
-            input(item.nbGuests);
+function getRecipe(id) {
+    allData.forEach(item => {
+        if (item.id == id) {
+            eachRecipe(item.name, item.iconUrl);
             eachIngredient(item.ingredients);
+            numberGuests(item.nbGuests);
+            eachInstructions(item.instructions);
         }
-    })
+    });
 }
-function eachRecipe(name,img){
-    var result ="";
-    result +=`
-    <h1>${name}</h1>
-    <img src ="${img}" width ="100">
+function eachRecipe(name, img) {
+    var result = "";
+    result += `
+        <div class="col-3"></div>
+        <div class="col-3"><h2>${name}</h2></div>
+        <div class="col-3"><img src="${img}" width="200px"></div>
+        <div class="col-3"></div>
     `;
     $('#recipe-result').html(result);
 }
 //get ingredient
-function eachIngredient(ingredients){
+$('#text-ingredient').hide();
+function eachIngredient(ingredients) {
     result = "";
     ingredients.forEach(element => {
         result += `
         <tr>
             <td><img src="${element.iconUrl}" class="img-fluid" width="50px"></td>
-            <td>${element.quantity + " " + element.unit[0] }</td>
+            <td>${element.quantity + " " + element.unit[0]}</td>
             <td>${element.name}</td>
         </tr>
         `;
     });
     $("#ingredient").html(result);
+    $('#text-ingredient').show();
 }
-function input(member){
+//
+function numberGuests(members) {
     var result = "";
-    result += `    
+    result += `
+        <div class="col-3"></div>
+        <div class="col-3"><h5>Number of persons</h5></div>
+        <div class="col-3">
+            <form action="#">
+                <div class="input-group mb-3 border">
+                    <div class="input-group-prepend">
+                        <button class="btn btn-light" id="minus" type="button">-</button>
+                    </div>
+                    <input type="text" id="persons" width="10px" value="${members}" min="1" max="15" disabled
+                        class="form-control text-center">
+                    <div class="input-group-append">
+                        <button class="btn btn-light" id="plus" type="button">+</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="col-3"></div>
     `;
-    $("#input").html(result);
+    $("#input-number").html(result);
+}
+//
+$('#text-instruction').hide();
+function eachInstructions(instructions) {
+    var split = instructions.split('<step>');
+    var result = "";
+    for (let i = 1; i < split.length; i++) {
+        result += `
+    <h6 class="text text-info">Step:${i}</h6>
+    <p>${split[i]}</p>
+    `;
+        $('#instruction').html(result);
+        $('#text-instruction').show();
+    }
+}
+//
+var inputValue;
+function increases(){
+    var value = $("#persons").val();
+    inputValue = parseInt(value) + 1;
+    if (inputValue <= 15){
+        $("#persons").val(inputValue);
+        
+    }
+}
+function descreases(){
+    var value = $("#persons").val();
+    inputValue = parseInt(value) - 1;
+    if (inputValue >= 1){
+        $("#persons").val(inputValue);
+    }        
 }
 
